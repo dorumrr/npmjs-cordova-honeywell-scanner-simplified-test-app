592e5caabdeb9d9f3e030c1871a85b207a76822c
Doru Moraru <info@doru.uk>
2019-04-06 01:31:25 +0100

    New tests for softwareTriggerStart and softwareTriggerStop

--- README.md
+++ README.md
@@ -2,20 +2,16 @@
 This is a dedicated Ionic application that implements the [cordova-honeywell-scanner-simplified](https://www.npmjs.com/package/cordova-honeywell-scanner-simplified) plugin for testing purposes.
 
 ### Install
-Ionic: `npm run dev-setup-ionic`
 
-Cordova: `npm run dev-setup-cordova`
+After cloning this repo, also clone [npmjs-cordova-honeywell-scanner-simplified](https://github.com/dorumrr/npmjs-cordova-honeywell-scanner-simplified)
+
+`npm run dev-setup`
+
 
 ### Run directly on device whenz connected via USB
 Run [`abd devices`](https://developer.android.com/studio/command-line/adb) in your terminal. Your device id should be listed (disconnect and reconnect if not listed).
 
-Ionic: `ionic cordova run android`
-
-Cordova: `cordova run android`
-
-Add `-l` to preview live changes.
+`npm start` OR `ionic cordova run android -l`
 
 ### Build apk
 Ionic: `ionic cordova build android`
-
-Cordova: `cordova build android`
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,12 +6,33 @@ import { Component, OnInit, NgZone } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor(private ngZone: NgZone){}
-
   window: any = window;
+
+  constructor(public ngZone: NgZone){}
+
   message: string =null;
   barcodes = [];
   status: string = 'Initialization... Are you on device?';
+
+  scanPressed () {
+    this.window.plugins.honeywell.softwareTriggerStart(data => {
+      console.log(`Software scan: ${data}`);
+      this.ngZone.run(() => {
+        this.barcodes = [`${data} @ ${new Date().toISOString().replace(/[T,Z]/g, " ").split('.')[0]}`, ...this.barcodes]
+      });
+    }, error => {
+      console.log(`Error occured: ${error}`);
+      this.ngZone.run(() => {
+        this.barcodes = [`${error} @ ${new Date().toISOString().replace(/[T,Z]/g, " ").split('.')[0]}`, ...this.barcodes]
+      });
+    });
+    // stop reader after five seconds if nothing scanned
+  }
+  scanReleased () {
+    this.window.plugins.honeywell.softwareTriggerStop();
+  }
+
+
 
   listen () {
     this.status = `enabled`;
